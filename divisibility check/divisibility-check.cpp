d065a1b9f3881c96726cedd611cd76846c1f2cad
Javlonbek Rasulov <javlonbekrasulovv@gmail.com>
2025-10-21 13:30:06 +0500

    divisibility checker
    A program that finds and prints all numbers between 100 and 500 that are divisible by both 3 and 5

---
+++ divisibility check/divisibility-check.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // A program that finds and prints all numbers between 100 and 500 that are divisible by both 3 and 5.
+    for (int i = 100; i <= 500; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            cout << i << endl;
+        }
+    }
+}

0383376b3319e8ee303acf805326d7812b3cfe74
Javlonbek Rasulov <javlonbekrasulovv@gmail.com>
2025-11-14 12:30:18 +0500

    average grade converter
    a program that converts/calculates the average grade of the students

---
+++ student avg grade/avgGrade.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+char gradeFunc(double avg){
+    if (avg >= 90) return 'A';
+    else if (avg >= 80) return 'B';
+    else if (avg >= 70) return 'C';
+    else if (avg >= 60) return 'D';
+    else return 'F';
+}
+
+int main(){
+    int count;
+    double sum = 0, grade;
+
+    cout << "How many grades (1 to 5)\n";
+    cin >> count;
+
+    if (count < 1 || count > 5){
+        cout << "You entered the wrong number of grades. Try again.\n";
+        return 1 ;
+    }
+
+    for (int i = 1; i <= count; i++){
+        cout << "Enter the grade " << i << ": ";
+        cin >> grade;
+        sum += grade;
+    }
+
+    double avg = sum / count;
+
+    cout << endl << "Your average grade is: " << avg << endl;
+
+    cout << "Your letter grade is: " << gradeFunc(avg);
+}
